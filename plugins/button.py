02bfa9834c92dbb2f8b4862511b73a2720437105
asepganz <149903249+asepganz@users.noreply.github.com>
2025-03-11 16:52:22 +0800

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -28,10 +28,11 @@ def start_button(client):
     if current_row:
         dynamic_buttons.append(current_row)
 
-    buttons = [
+    buttons = 
+    [
         dynamic_buttons + [
         [InlineKeyboardButton(text="ᴛᴜᴛᴜᴘ", callback_data="close")],
-    ]
+     ]
     ]
     return buttons
 
